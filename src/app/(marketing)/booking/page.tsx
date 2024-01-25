7a6ffde8f1ac2350ef395cd289f9feee7a3c4062
moonGoblin <kaungzaw1996@gmail.com>
2024-01-25 15:49:04 +1100

    fixed sitemap

--- src/app/(marketing)/booking/page.tsx
+++ src/app/(marketing)/booking/page.tsx
@@ -4,9 +4,15 @@ import {
     PageHeaderHeading,
 } from '@/components/page-header';
 import { Shell } from '@/components/shell';
+import { Metadata } from 'next';
 import React from 'react';
 
 export const runtime = 'edge';
+
+export const metadata: Metadata = {
+    title: 'Book Now',
+    description: 'Book online in less than 60 seconds.',
+};
 
 export default function Page() {
     return (
